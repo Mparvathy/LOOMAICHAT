333e8f234fba082c138fe398b6f4eabcf167e9bc
PARVATHY M <167396096+Mparvathy@users.noreply.github.com>
2026-08-16 15:22:11 +0530

    Latest CODE

--- project/project/settings.py
+++ project/project/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
